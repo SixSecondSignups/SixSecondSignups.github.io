93ef4e5b06bd030eac3d69161f5f0dc4a88e2a9b
Abhinav Vadrevu <abhinavvadrevu1@gmail.com>
2020-05-31 22:37:19 -0700

    there's supposedly mixpanel tracking

---
+++ tracking.js
@@ -0,0 +1,23 @@
+function loadPage() {
+  mixpanel.track("Page Load");
+}
+
+function clickCTA() {
+  mixpanel.track("CTA Click");
+}
+
+function privacy() {
+  mixpanel.track("Privacy");
+}
+
+function features() {
+  mixpanel.track("features");
+}
+
+function howitworks() {
+  mixpanel.track("howitworks");
+}
+
+function pricing() {
+  mixpanel.track("pricing");
+}
